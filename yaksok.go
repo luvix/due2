7bd8fdaf15e51ff3bf044efc7861b9df9005b398
Theodore Leesuk Kim <leesuk.kim425@gmail.com>
2018-01-17 21:52:36 +0900

    windows test

--- yaksok.go
+++ yaksok.go
@@ -3,79 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os/exec"
 )
 
-// YaksokFlagSetInterface is an interface for the struct FlagSet.
-type YaksokFlagSetInterface interface {
-	// initialize FlagSet
-	init()
-	// 명령어를 처리한다.
-	pitin()
-}
-
-type AddFlagSet struct {
-	YaksokFlagSetInterface
+// SecondlyFlagSet ...
+type SecondlyFlagSet struct {
 	flagSet flag.FlagSet
 }
 
-// This is  AddFlagSet.init
-func (flagset *AddFlagSet) init() {
-	flagset.String("at", "", "Schedule the time when a job is run.")
-	flagset.String("day", "", "Schedule the time when a job is run daily.")
-	flagset.String("week", "", "Schedule the date when a job is run weekly.")
-	flagset.String("month", "", "Schedule the date or the weekday when a job is run monthly.")
-	flagset.String("year", "", "Schedule the date or weekdate or monthdate when a job is run yearly.")
-}
-
-type ListFlagSet struct {
-	YaksokFlagSetInterface
-}
-
-// This is  ListFlagSet.init
-func (flagset *ListFlagSet) init() {
-	flagset.String("list", "", "Load yaksokduled job list. The list is owned by user.")
-	flagset.String("list-all", "", "Load yaksokdule job list. The job list is readable by root. This command can run only sudoers.")
-}
-
-type DeleteFlagSet struct {
-	YaksokFlagSetInterface
-}
-
-// This is  DeleteFlagSet.init
-func (flagset *DeleteFlagSet) init() {
-	flagset.String("delete")
-}
-
-type PreferenceFlagSet struct {
-	YaksokFlagSetInterface
-}
-
-// This is  PreferenceFlagSet.init
-func (flagset *PreferenceFlagSet) init() {
-
+// This is  SecondlyFlagSet.init
+func (I *SecondlyFlagSet) init() {
+	I.flagSet.String("job", "", "command what you want to schedule. If your job command is next to keyword, option is not to be needed.")
 }
 
 func main() {
 	// Subcommands
-	addFlagSet := flag.NewFlagSet("add", flag.ExitOnError)
-	listFlagSet := flag.NewFlagSet("list", flag.ExitOnError)
-	deleteFlagSet := flag.NewFlagSet("delete", flag.ExitOnError)
-	preferenceFlagSet := flag.NewFlagSet("preference", flag.ExitOnError)
-
-	AddFlagSet.init()
-
-	// Count subcommand flag pointers
-	// Adding a new choice for --metric of 'substring' and a new --substring flag
-	countTextPtr := addFlagSet.String("text", "", "Text to parse. (Required)")
-	countMetricPtr := countCommand.String("metric", "chars", "Metric {chars|words|lines|substring}. (Required)")
-	countSubstringPtr := countCommand.String("substring", "", "The substring to be counted. Required for --metric=substring")
-	countUniquePtr := countCommand.Bool("unique", false, "Measure unique values of a metric.")
-
-	// List subcommand flag pointers
-	listTextPtr := listCommand.String("text", "", "Text to parse. (Required)")
-	listMetricPtr := listCommand.String("metric", "chars", "Metric <chars|words|lines>. (Required)")
-	listUniquePtr := listCommand.Bool("unique", false, "Measure unique values of a metric.")
-
+	secondlyFlagSet := flag.NewFlagSet("secondly", flag.ExitOnError)
 	// Verify that a subcommand has been provided
 	// os.Arg[0] is the main command
 	// os.Arg[1] will be the subcommand
@@ -91,44 +34,41 @@ func main() {
 	flag.Parse()
 
 	fmt.Println("flag.Args()", flag.Args())
-	fmt.Println("*listCommand: ", *listCommand)
-	fmt.Println("listCommand: ", listCommand)
+	fmt.Println("*listCommand: ", *secondlyFlagSet)
+	fmt.Println("listCommand: ", secondlyFlagSet)
 
 	fmt.Println("flag.args")
-	for i, v := range flag.Args() {
+	flagargs := flag.Args
+	for i, v := range flagargs() {
 		fmt.Println(i, v)
 	}
 
-	fmt.Println("countcommand.args")
-	for i, v := range countCommand.Args() {
+	secondlyFlagSet.Parse(flag.Args()[1:])
+
+	fmt.Println("secondlyFlagSet.args")
+	for i, v := range secondlyFlagSet.Args() {
 		fmt.Println(i, v)
 	}
 
-	fmt.Println("countCommand.Parsed", countCommand.Parsed())
+	fmt.Println("secondlyFlagSet.Parsed", secondlyFlagSet.Parsed())
 
-	switch flag.Arg(0) {
-	case "count":
-		countCommand.Parse(flag.Args()[1:])
-	case "list":
-		listCommand.Parse(flag.Args()[1:])
-	}
-
-	fmt.Println("*countCommand: ", *countCommand)
-	fmt.Println("*countTextPtr: ", *countTextPtr)
-	fmt.Println("*countMetricPtr: ", *countMetricPtr)
-	fmt.Println("*countSubstringPtr: ", *countSubstringPtr)
-	fmt.Println("*countUniquePtr: ", *countUniquePtr)
-	fmt.Println("*listTextPtr: ", *listTextPtr)
-	fmt.Println("*listMetricPtr: ", *listMetricPtr)
-	fmt.Println("*listUniquePtr: ", *listUniquePtr)
-	// switch os.Args[1] {
-	// case "list":
-	// 	listCommand.Parse(os.Args[2:])
+	// switch flag.Arg(0) {
 	// case "count":
-	// 	countCommand.Parse(os.Args[2:])
-	// default:
-	// 	flag.PrintDefaults()
-	// 	os.Exit(1)
+	// 	secondlyFlagSet.Parse(flag.Args()[1:])
+	// case "list":
+	// 	secondlyFlagSet.Parse(flag.Args()[1:])
 	// }
-	fmt.Println("hell world")
+
+	for _, arg := range secondlyFlagSet.Args() {
+		// var temp flag.FlagSet
+		// temp.Parse(strings.Split(temp, " "))
+		fmt.Println(arg)
+		cmd := exec.Command("cmd", "/C", "echo", "hello")
+		// var out bytes.Buffer
+		// cmd.Stdin = strings.NewReader("some input")
+		// cmd.Stdout = &out
+		// cmd.Run()
+		output, _ := cmd.Output()
+		fmt.Printf("in all caps: %q", output)
+	}
 }
