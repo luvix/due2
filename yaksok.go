f8cfd43644f92d677cfa208e2b742ed3195edb5e
Theodore Leesuk Kim <leesuk.kim425@gmail.com>
2018-01-31 21:59:45 +0900

    update yaksok

--- yaksok.go
+++ yaksok.go
@@ -20,77 +20,88 @@ func Usage() {
 }
 
 //Yaksok is an object model for main level flags
-type Yaksok struct {
-	version  *bool
-	help     *bool
-	once     *flag.FlagSet
-	daily    *flag.FlagSet
-	weekly   *flag.FlagSet
-	monthly  *flag.FlagSet
-	yearly   *flag.FlagSet
-	secondly *flag.FlagSet
-	minutely *flag.FlagSet
-	hourly   *flag.FlagSet
-	list     *flag.FlagSet
-	delete   *flag.FlagSet
-	setting  *flag.FlagSet
-	// subsets  map[string]*flag.FlagSet
+type MainFlagBox struct {
+	version *bool
+	help    *bool
 }
 
-// YaksokFactory initliazes Yaksok.
-func YaksokFactory() *Yaksok {
-	ys := new(Yaksok)
-	ys.once = flag.NewFlagSet("once", flag.PanicOnError)
-	ys.daily = flag.NewFlagSet("daily", flag.PanicOnError)
-	// ys.weekly = flag.NewFlagSet("weekly", flag.PanicOnError)
-	// ys.monthly = flag.NewFlagSet("monthly", flag.PanicOnError)
-	// ys.yearly = flag.NewFlagSet("yearly", flag.PanicOnError)
-	ys.secondly = flag.NewFlagSet("secondly", flag.PanicOnError)
-	ys.minutely = flag.NewFlagSet("minutely", flag.PanicOnError)
-	ys.hourly = flag.NewFlagSet("hourly", flag.PanicOnError)
-	ys.list = flag.NewFlagSet("list", flag.PanicOnError)
-	ys.delete = flag.NewFlagSet("delete", flag.PanicOnError)
-	ys.setting = flag.NewFlagSet("setting", flag.PanicOnError)
-
-	return ys
+type SubFlagBox struct {
+	//TODO: 곧 만들꺼야
 }
 
-//ShakeFlag is shaking yaksok flag to indicate what yaksok core would be done.
-func (flag *Yaksok) ShakeFlag(args []string) {
+type FlagBox struct {
+	mainBox *MainFlagBox
+	subBox  *SubFlagBox
+}
+
+func MainFlagBoxFactory() *MainFlagBox {
+	return new(MainFlagBox)
+}
+
+func SubFlagBoxFactory() *SubFlagBox {
+	return new(SubFlagBox)
+}
+
+// FlagBoxFactory initliazes FlagBox.
+func FlagBoxFactory() *FlagBox {
+	box := new(FlagBox)
+	box.mainBox = MainFlagBoxFactory()
+	box.subBox = SubFlagBoxFactory()
+
+	return box
+}
+
+//PickupMainFlag is shaking yaksok flag to indicate what yaksok core would be done.
+func (box *FlagBox) Pickup(args []string) {
 	if len(args) > 0 {
 		// if argument is subflagset
-		flag.ShakeSubsetFlag(args)
+		box.subBox.Pickup(args)
 	} else {
-		// if argument is main flag
+		// if argument is main flag withoutsubflags
+		box.mainBox.Pickup()
 	}
 }
 
-//ShakeSubsetFlag is shaking subset flag of yaksok to indicate what yaksok core would be done.
-func (flag *Yaksok) ShakeSubsetFlag(args []string) int {
+func (box *MainFlagBox) Pickup() {
+	if *box.version {
+		fmt.Println(YaksokVersion)
+	} else {
+		if !*box.help {
+			fmt.Fprintf(os.Stderr, "Yaksok needs flag or command.\n")
+		}
+		Usage()
+	}
+}
+
+func (box *SubFlagBox) Pickup(args []string) {
+	if len(args) == 0 {
+		return
+	}
+
 	switch args[0] {
 	case "secondly":
 		fmt.Println("sec", args)
-		flag.secondly.Parse(args[1:])
-		fmt.Println(flag.secondly.Args())
+		// flag.secondly.Parse(args[1:])
+		// fmt.Println(flag.secondly.Args())
 	case "daily":
-		flag.daily.Parse(args[1:])
-		fmt.Println(flag.daily.Args())
+		// flag.daily.Parse(args[1:])
+		// fmt.Println(flag.daily.Args())
 	default:
 		fmt.Println("엄....")
 	}
-	return 1
 }
 
 func main() {
-	fVersion := flag.Bool("v", false, "Read the module version.")
-	fHelp := flag.Bool("h", false, "Gel help")
+	box := FlagBoxFactory()
+	box.mainBox.version = flag.Bool("v", false, "Read the module version.")
+	box.mainBox.help = flag.Bool("h", false, "Gel help")
 
 	// fsOnce
-	fsDaily := flag.NewFlagSet("daily", flag.ExitOnError)
+	// fsDaily := flag.NewFlagSet("daily", flag.ExitOnError)
 	// fsWeekly
 	// fsMonthly
 	// fsYearly
-	fsSecondly := flag.NewFlagSet("secondly", flag.ExitOnError)
+	// fsSecondly := flag.NewFlagSet("secondly", flag.ExitOnError)
 	// secondlyAt := fsSecondly.String("at", time.Now().Format(time.RFC3339), "the time when you want to run a job")
 	// fsMinutely
 	// fsHourly
@@ -104,26 +115,28 @@ func main() {
 	// parse main flag
 	flag.Parse()
 
+	box.Pickup(flag.Args())
+
 	// if argument is exist: user want to use flagset
-	if len(flag.Args()) > 0 {
-		switch flag.Arg(0) {
-		case "secondly":
-			fsSecondly.Parse(flag.Args()[1:])
-			fmt.Println(fsSecondly.Args())
-		case "daily":
-			fsDaily.Parse(flag.Args()[1:])
-			fmt.Println(fsDaily.Args())
-		default:
-			fmt.Println("엄....")
-		}
-	} else {
-		if *fVersion {
-			fmt.Println(YaksokVersion)
-		} else if *fHelp {
-			Usage()
-		} else {
-			fmt.Fprintf(os.Stderr, "Yaksok needs flag or command.\n")
-			flag.PrintDefaults()
-		}
-	}
+	// if len(flag.Args()) > 0 {
+	// 	switch flag.Arg(0) {
+	// 	case "secondly":
+	// 		fsSecondly.Parse(flag.Args()[1:])
+	// 		fmt.Println(fsSecondly.Args())
+	// 	case "daily":
+	// 		fsDaily.Parse(flag.Args()[1:])
+	// 		fmt.Println(fsDaily.Args())
+	// 	default:
+	// 		fmt.Println("엄....")
+	// 	}
+	// } else {
+	// 	if *fVersion {
+	// 		fmt.Println(YaksokVersion)
+	// 	} else if *fHelp {
+	// 		Usage()
+	// 	} else {
+	// 		fmt.Fprintf(os.Stderr, "Yaksok needs flag or command.\n")
+	// 		flag.PrintDefaults()
+	// 	}
+	// }
 }
--- yaksok_test.go
+++ yaksok_test.go
@@ -6,56 +6,47 @@ import (
 )
 
 // main 함수를 테스트 할 수 있는 함수는 이것 뿐이다.
+//그런데 놀랍게도 이 함수는 flag.Parse를 호출하지 않기때문에 쓸모없다.
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestYaksokFactory(t *testing.T) {
-	ys := YaksokFactory()
+func TestMainFlagBoxFactory(t *testing.T) {
+	box := MainFlagBoxFactory()
 
-	if ys.once == nil {
-		t.Error()
+	if box == nil {
+		t.Error("New box fail")
 	}
-	if ys.daily == nil {
-		t.Error()
-	}
-	if ys.weekly == nil {
-		t.Error()
-	}
-	if ys.monthly == nil {
-		t.Error()
-	}
-	if ys.yearly == nil {
-		t.Error()
-	}
-	if ys.secondly == nil {
-		t.Error()
-	}
-	if ys.minutely == nil {
-		t.Error()
-	}
-	if ys.hourly == nil {
-		t.Error()
-	}
-	if ys.list == nil {
-		t.Error()
-	}
-	if ys.minutely == nil {
-		t.Error()
-	}
-	if ys.setting == nil {
-		t.Error()
-	}
-
 }
 
-func TestShakeFlag(t *testing.T) {
-	yaksok := YaksokFactory()
-	yaksok.ShakeFlag([]string{"h"})
-	yaksok.ShakeFlag([]string{"v"})
+func TestSubFlagBoxFactory(t *testing.T) {
+	box := SubFlagBoxFactory()
+
+	if box == nil {
+		t.Error("New box fail")
+	}
 }
 
-func TestShakeSubsetFlag(t *testing.T) {
-	yaksok := YaksokFactory()
-	yaksok.ShakeSubsetFlag([]string{"secondly", "echo \"hello world\""})
+func TestFlagBoxFactory(t *testing.T) {
+	box := FlagBoxFactory()
+
+	if box == nil {
+		t.Error("New box fail")
+	}
+}
+
+func TestFlagBoxPickup(t *testing.T) {
+	box := FlagBoxFactory()
+	box.Pickup(nil)
+}
+
+func TestMainFlagBoxPickup(t *testing.T) {
+	box := MainFlagBoxFactory()
+	box.Pickup()
+}
+
+func TestSubFlagBoxPickup(t *testing.T) {
+	box := SubFlagBoxFactory()
+
+	box.Pickup(nil)
 }
